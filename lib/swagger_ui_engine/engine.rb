2e3adcaba3875f5202f3f45ca002f835525c5f84
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2017-03-16 14:19:08 +0100

    Move Swagger assets from public to assets

--- lib/swagger_ui_engine/engine.rb
+++ lib/swagger_ui_engine/engine.rb
@@ -2,9 +2,17 @@ module SwaggerUiEngine
   class Engine < ::Rails::Engine
     isolate_namespace SwaggerUiEngine
 
-    # Initializer to combine this engines static assets with the static assets of the hosting site.
-    initializer 'static assets' do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+    config.to_prepare do
+      Rails.application.config.assets.precompile += %w(
+        swagger_ui_engine/typography.css
+        swagger_ui_engine/reset.css
+        swagger_ui_engine/screen.css
+        swagger_ui_engine/print.css
+        logo_small.png
+      )
+
+      Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+      Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     end
   end
 
