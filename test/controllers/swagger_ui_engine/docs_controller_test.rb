83967c2480ef3cd1cdb8005ab4a2aa914fd3adfa
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2017-04-18 15:54:03 +0200

    Test configurable options

--- test/controllers/swagger_ui_engine/docs_controller_test.rb
+++ test/controllers/swagger_ui_engine/docs_controller_test.rb
@@ -13,5 +13,18 @@ module SwaggerUiEngine
       assert_response :success
       assert_select '#swagger-ui-container'
     end
+
+    test 'custom config options should work successfully' do
+      get '/swagger'
+      assert_match('url = "api_docs/swagger.yaml"', @response.body)
+      assert_match('docExpansion: "list"', @response.body)
+      assert_match('defaultModelRendering: "model"', @response.body)
+    end
+
+    test 'default config options should work successfully' do
+      get '/swagger'
+      assert_match('showRequestHeaders: "false"', @response.body)
+      assert_match('jsonEditor: "false"', @response.body)
+    end
   end
 end
--- test/dummy/config/initializers/swagger_ui_engine.rb
+++ test/dummy/config/initializers/swagger_ui_engine.rb
@@ -1,3 +1,5 @@
 SwaggerUiEngine.configure do |config|
   config.swagger_url = 'api_docs/swagger.yaml'
+  config.doc_expansion = 'list'
+  config.model_rendering = 'model'
 end
