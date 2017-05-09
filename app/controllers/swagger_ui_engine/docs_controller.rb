75126fc67e1df7cea6d8c627542557f0895c91c3
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2017-05-09 10:18:23 +0200

    Add backward compatibility for doc urls defined in string

--- app/controllers/swagger_ui_engine/docs_controller.rb
+++ app/controllers/swagger_ui_engine/docs_controller.rb
@@ -6,11 +6,12 @@ module SwaggerUiEngine
     before_action :set_configs
 
     def index
-      redirect_to doc_path(@swagger_url.keys.first) if single_doc_version?
+      redirect_to doc_path('v1') if single_doc_url?
+      redirect_to doc_path(@swagger_url.keys.first) if single_doc_url_hash?
     end
 
     def show
-      @swagger_url = @swagger_url[params[:id].to_sym]
+      @swagger_url = @swagger_url[params[:id].to_sym] unless single_doc_url?
     end
 
     private
@@ -24,8 +25,12 @@ module SwaggerUiEngine
       @validator_url = set_validator_url
     end
 
-    def single_doc_version?
-      @swagger_url.size == 1
+    def single_doc_url?
+      @swagger_url.is_a?(String)
+    end
+
+    def single_doc_url_hash?
+      @swagger_url.is_a?(Hash) && @swagger_url.size == 1
     end
   end
 end
