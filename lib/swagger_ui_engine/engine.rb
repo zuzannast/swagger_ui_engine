3cff9c4a15d223c11843f941a60ca26e701b5873
Zuzanna Stolińska <zuzanna.stolinska@netguru.pl>
2019-04-29 09:06:44 +0200

    Change Rails runtime dependency version

--- lib/swagger_ui_engine/engine.rb
+++ lib/swagger_ui_engine/engine.rb
@@ -5,7 +5,7 @@ module SwaggerUiEngine
     isolate_namespace SwaggerUiEngine
 
     config.to_prepare do
-      Rails.application.config.assets.precompile += %w(
+      Rails.application.config.assets.precompile += %w[
         swagger_ui_engine/lib/typography.css
         swagger_ui_engine/lib/reset.css
         swagger_ui_engine/lib/screen.css
@@ -18,7 +18,7 @@ module SwaggerUiEngine
         swagger_ui_engine/lang/*.js
         swagger_ui_engine/DroidSans-Bold.ttf
         swagger_ui_engine/DroidSans.ttf
-      )
+      ]
 
       Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     end
